97fc32d6dd31948d569ce159b4d439b63b813514
Optiplex 3080 <im.rochamatheus@gmail.com>
2022-09-10 18:12:37 -0300

    feat: add 'frase' class

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
-  styleUrls: ['./tentativas.component.css']
+  styleUrls: ['./tentativas.component.css'],
 })
 export class TentativasComponent implements OnInit {
+  public coracaoVazio: string = '/assets/img/coracao_vazio.png';
+  public coracaoCheio: string = '/assets/img/coracao_cheio.png';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
