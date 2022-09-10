343baa9bace04db1481f55bcc3e75eaa7a7a1ee8
Optiplex 3080 <im.rochamatheus@gmail.com>
2022-09-10 18:19:24 -0300

    feat: Add game phrases

---
+++ src/app/painel/frase-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model';
+
+export const FRASES: Frase[] = [
+  { fraseEng: 'How are you?', frasePtBr: 'Como está você?' },
+  { fraseEng: 'I eat bread', frasePtBr: 'Eu como pão' },
+  { fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender' },
+  { fraseEng: 'I watch TV', frasePtBr: 'Eu assisto TV' },
+];
--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Frase } from '../shared/frase.model';
+import { FRASES } from './frase-mock';
 
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
-  styleUrls: ['./painel.component.css']
+  styleUrls: ['./painel.component.css'],
 })
 export class PainelComponent implements OnInit {
+  public frases: Frase[] = FRASES;
+  public instrucao: string = 'Traduza a expressão: ';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
