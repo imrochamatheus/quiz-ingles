1772f060a4ae16bd5fbe71da19f3a0e2ca7c4f0e
Optiplex 3080 <im.rochamatheus@gmail.com>
2022-09-10 17:51:43 -0300

    initial commit

---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topo',
+  templateUrl: 'topo.component.html',
+  styleUrls: ['./topo.component.css'],
+})
+export class TopoComponent {
+  constructor() {}
+
+  public title: string = 'Aprendendo inglês';
+}
